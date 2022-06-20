b28597574d5e14e05c70318226998bbf7c0dd6ca
Pawan Kumar <surpawan@gmail.com>
2022-06-20 20:09:28 +0530

    Added Changes to slider

--- components/SwiperSection.jsx
+++ components/SwiperSection.jsx
@@ -18,70 +18,81 @@ const SwiperSection = () => {
           delay: 5000,
           disableOnInteraction: false,
         }}
-        className="h-full lg:h-128 z-10"
+        className="h-60 lg:h-128 z-10"
         slidesPerView={1}
       >
         <SwiperSlide>
-          <div className="relative z-10">
-            <div className="absolute bottom-0 bg-gradient-to-r from-white/90 to-transparent p-24 z-50 top-0 left-0 right-0">
-              <div className="text-primary text-lg lg:text-xl mb-2 lg:mb-4 flex items-center lg:space-x-2">
+          <div className="relative h-60 lg:h-128 z-10">
+            <div className="absolute bottom-0 p-6 lg:p-24 z-50 top-0 left-0 right-0">
+              <div className="text-primary text-sm lg:text-xl mb-2 lg:mb-4 flex items-center lg:space-x-2">
                 <Dash />
                 <div>Welcome to Azra - Your Personal Dietitian</div>
               </div>
-              <div className="text-xl lg:text-7xl font-title w-full mb-4">
+              <div className="text-2xl lg:text-7xl font-title w-full mb-2 lg:mb-4">
                 Easy to Follow <br /> Diet Plans from Rs 1730
               </div>
-              <div className="text-lg lg:text-xl text-black mb-4">
-                Weight Loss | Weight Gain | Hypertension | Pregnancy <br />|
-                PCOS | Diabetes | Renal | Thyroid | Child-Nutrition | Sports
-                Nutrition
+              <div className="text-xs lg:text-xl text-black mb-2 lg:mb-4">
+                Weight Loss | Weight Gain | Hypertension | Pregnancy{' '}
+                <br className="hidden lg:flex" />| PCOS | Diabetes | Renal |
+                Thyroid | Child-Nutrition | Sports Nutrition
               </div>
-              <WhatsAppButton />
+              <WhatsAppButton classes="bg-primary text-white text-sm lg:text-base rounded-3xl px-2 py-1 lg:px-4 lg:py-2 space-x-2 items-center inline-flex" />
             </div>
-            <Image src={sliderOne} alt="Slide One" />
+            <Image
+              src={sliderOne}
+              layout="fill"
+              objectFit="cover"
+              alt="Slide One"
+            />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="relative">
-            <div className="absolute bottom-0 bg-gradient-to-r from-white/90 to-transparent p-24 z-50 top-0 left-0 right-0">
-              <div className="text-primary text-lg lg:text-xl mb-2 lg:mb-4 flex items-center lg:space-x-2">
+          <div className="relative h-60 lg:h-128">
+            <div className="absolute bottom-0  p-6 lg:p-24 z-50 top-0 left-0 right-0">
+              <div className="text-primary text-sm lg:text-xl mb-2 lg:mb-4 flex items-center lg:space-x-2">
                 <Dash />
                 Start Your Heath Journey Now
               </div>
-              <div className="text-7xl font-title w-full mb-4">
+              <div className="text-xl lg:text-7xl font-title w-full mb-2 lg:mb-4">
                 Eat <span className="text-primary">Right</span> <br />
                 Stay Bright
               </div>
-              <div className="text-xl text-black mb-4">
+              <div className="text-xs lg:text-xl text-black mb-4">
                 Simple Diet Plans - That Work Wonders!
               </div>
-              <button className="bg-primary text-white rounded-3xl px-4 py-2">
-                WhatsApp Senior Dietitian Now
-              </button>
+              <WhatsAppButton classes="bg-primary text-white text-sm lg:text-base rounded-3xl px-2 py-1 lg:px-4 lg:py-2 space-x-2 items-center inline-flex" />
             </div>
-            <Image src={sliderTwo} alt="Slide One" />
+            <Image
+              src={sliderTwo}
+              layout="fill"
+              objectFit="cover"
+              alt="Slide One"
+            />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="relative">
-            <div className="absolute bottom-0 bg-gradient-to-r from-white/90 to-transparent p-24 z-50 top-0 left-0 right-0">
-              <div className="text-primary text-lg lg:text-xl mb-2 lg:mb-4 flex items-center lg:space-x-2">
+          <div className="relative h-60 lg:h-128">
+            <div className="absolute bottom-0 bg-gradient-to-r from-white/90 to-transparent p-6 lg:p-24 z-50 top-0 left-0 right-0">
+              <div className="text-primary text-sm lg:text-xl mb-2 lg:mb-4 flex items-center lg:space-x-2">
                 <Dash />
                 Health is in your hands
               </div>
-              <div className="text-7xl font-title w-full mb-4">
+              <div className="text-xl lg:text-7xl font-title w-full mb-2 lg:mb-4">
                 15000+ <span className="text-primary px-2">Satisfied</span>
                 <br />
                 Healthy clients
               </div>
-              <div className="text-xl text-black mb-4">
+              <div className="text-xs lg:text-xl text-black mb-4">
                 Personal Nutritionist for every need
               </div>
-              <button className="bg-primary text-white rounded-3xl px-4 py-2">
-                WhatsApp Senior Dietitian Now
-              </button>
+              <WhatsAppButton classes="bg-primary text-white text-sm lg:text-base rounded-3xl px-2 py-1 lg:px-4 lg:py-2 space-x-2 items-center inline-flex" />
             </div>
-            <Image src={sliderThree} alt="Slide One" />
+            <Image
+              src={sliderThree}
+              layout="fill"
+              objectFit="cover"
+              alt="Slide One"
+            />
           </div>
         </SwiperSlide>
       </Swiper>
