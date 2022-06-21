d8513135c210b720676b174e376e15a64107e71c
Pawan Kumar <surpawan@gmail.com>
2022-06-21 18:57:06 +0530

    Fixed whatsapp button

--- components/WhatsAppButton.jsx
+++ components/WhatsAppButton.jsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const WhatsAppButton = ({
   classes = 'bg-primary text-white rounded-3xl px-4 py-2 space-x-2 items-center inline-flex',
   text = 'WhatsApp Senior Dietitian Now',
+  hideOnMobile = false,
 }) => {
   return (
     <a
@@ -13,7 +14,9 @@ const WhatsAppButton = ({
       rel="noreferrer"
     >
       <FontAwesomeIcon size="2x" icon={faWhatsapp} fixedWidth />
-      <div className="hidden lg:flex">{text}</div>
+      <div className={`${hideOnMobile ? 'hidden lg:flex' : 'flex'}`}>
+        {text}
+      </div>
     </a>
   )
 }
--- components/layout.js
+++ components/layout.js
@@ -50,6 +50,7 @@ const Layout = ({ children }) => {
           <WhatsAppButton
             classes="bg-primary text-white rounded-l-3xl px-4 py-2 space-x-2 items-center inline-flex"
             text="WhatsApp Senior Dietitian"
+            hideOnMobile={true}
           />
         </div>
       </div>
