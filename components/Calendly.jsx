a9e660a3e18e0a433e619a566a8eb3c257bd2a98
Pawan Kumar <surpawan@gmail.com>
2022-06-22 16:58:34 +0530

    Added Changes

--- components/Calendly.jsx
+++ components/Calendly.jsx
@@ -1,24 +1,21 @@
-import Script from 'next/script'
+import Link from 'next/link'
 
 const Calendly = () => {
   return (
-    <>
-      <Script
-        src="https://assets.calendly.com/assets/external/widget.js"
-        strategy="afterInteractive"
-      />
-      <div className="bg-white py-10 lg:py-28 px-4 lg:px-0">
-        <div className="container mx-auto">
-          <div className="text-center font-title text-4xl mb-10">
-            Schedule Free Consultation with Senior Nutritionist
-          </div>
-          <div
-            className="calendly-inline-widget w-full h-188 lg:h-160"
-            data-url="https://calendly.com/azrawellnessexpert/30min?&hide_gdpr_banner=1"
-          ></div>
+    <div className="bg-white py-10 lg:py-16 px-4 lg:px-0">
+      <div className="container mx-auto">
+        <div className="text-center font-title text-4xl mb-10">
+          Schedule Free Consultation with Senior Nutritionist
+        </div>
+        <div className="flex items-center justify-center">
+          <Link href="/schedule-consultation">
+            <a className="bg-primary px-4 text-center py-2 rounded shadow text-xl text-white">
+              Book Consultation
+            </a>
+          </Link>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
