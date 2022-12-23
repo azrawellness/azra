6a543a44bfccd4341b25188bdebd958f936a88d1
Aditi Gemini <aditi152003@gmail.com>
2022-12-23 16:42:36 +0530

    azrah

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,22 @@
-// import { initializeApp } from 'firebase/app'
-// import { getFirestore } from 'firebase/firestore'
-// import { getStorage } from 'firebase/storage'
-// import { getAuth } from 'firebase/auth'
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+import { getAuth } from 'firebase/auth'
 
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-//  }
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+ }
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig)
-// const db = getFirestore(app)
-// const auth = getAuth(app)
-// const storage = getStorage(app, `gs://${firebaseConfig.storageBucket}`)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const auth = getAuth(app)
+const storage = getStorage(app, `gs://${firebaseConfig.storageBucket}`)
 
-// export { auth, db, storage }
+export { auth, db, storage }
