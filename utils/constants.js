31513effdc2beed74e1703dd5911bf39d720ec6a
Aditi Gemini <aditi152003@gmail.com>
2023-02-06 01:01:33 +0530

    servicesRemoved

--- utils/constants.js
+++ utils/constants.js
@@ -22,11 +22,11 @@ export const NAVBAR_LINKS = [
   //   name: 'Yoga Live Online Classes',
   //   match: true,
   // },
-  {
-    href: '/services',
-    name: 'Services',
-    match: true,
-  },
+  // {
+  //   href: '/services',
+  //   name: 'Services',
+  //   match: true,
+  // },
   // {
   //   href: '/blog',
   //   name: 'Blog',
@@ -37,11 +37,11 @@ export const NAVBAR_LINKS = [
     name: 'Client Reviews',
     match: false,
   },
-  {
-    href: '/contact-us',
-    name: 'Contact Us',
-    match: false,
-  },
+  // {
+  //   href: '/contact-us',
+  //   name: 'Contact Us',
+  //   match: false,
+  // },
 ]
 
 export const SIDEBAR_LINKS = [
