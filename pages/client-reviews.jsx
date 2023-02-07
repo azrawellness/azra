4b3d6d71036cc884359cd90e1d7fee770e644ece
Aditi Gemini <aditi152003@gmail.com>
2023-02-07 11:05:14 +0530

    FormSpce

--- pages/client-reviews.jsx
+++ pages/client-reviews.jsx
@@ -31,7 +31,7 @@ const ClientReviews = () => {
   return (
     <div className="bg-gray text-black">
       <Header title="Client Reviews" />
-      <div className="container mx-auto py-10">
+      <div className="container mx-auto ">
         {loading ? (
           <Splash />
         ) : (
