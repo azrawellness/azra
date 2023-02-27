4bc56fb873836e7167cdc6f2d23446a2f3f83868
Aditi Gemini <aditi152003@gmail.com>
2023-02-27 12:01:27 +0530

    removedTongles

--- components/home/HomeTongles.jsx
+++ components/home/HomeTongles.jsx
@@ -4,7 +4,7 @@ import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import { faCalendarDays } from "@fortawesome/free-regular-svg-icons";
 export const HomeTongles = () => {
   return (
-    <div className="   fixed top-[35vh] md:top-[77vh] z-[200] right-0  flex flex-col gap-1 overflow-hidden ">
+    <div className="   fixed top-[40vh] md:top-[65vh] z-[200] right-0  flex flex-col gap-1 overflow-hidden ">
       <div className=" bg-primary  w-[20rem] h-[3rem] flex items-center toggles">
         <div className="bg-secondary h-[100%]  w-[20%] flex justify-center items-center toggleicons">
           {" "}
@@ -17,7 +17,7 @@ export const HomeTongles = () => {
           <a href="tel: 09899191936"> Call me</a>
         </button>
       </div>
-      <div className="bg-primary w-[20rem] h-[3rem] flex items-center toggles">
+      {/* <div className="bg-primary w-[20rem] h-[3rem] flex items-center toggles">
         <div className="bg-secondary h-[100%] w-[20%] flex justify-center items-center">
           {" "}
           <FontAwesomeIcon
@@ -40,7 +40,7 @@ export const HomeTongles = () => {
         <button className="w-[80%] text-white font-semibold text-lg text-center">
           <a href="https://wa.me/09899191936">Live Chat on Whatsapp</a>
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
