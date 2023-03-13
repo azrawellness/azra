e7a3cdda63dd6eb09e3f149cb90f9f55a812815e
Aditi Gemini <aditi152003@gmail.com>
2023-03-13 17:50:59 +0530

    contact

--- utils/constants.js
+++ utils/constants.js
@@ -1,15 +1,15 @@
-export const TESTIMONIALS = 'testimonials'
-export const POSTS = 'posts'
-export const SERVICES = 'services'
-export const CATEGORIES = 'categories'
-export const TAGS = 'tags'
-export const USERS = 'users'
-export const CLIENT_REVIEWS = 'testimonials'
+export const TESTIMONIALS = "testimonials";
+export const POSTS = "posts";
+export const SERVICES = "services";
+export const CATEGORIES = "categories";
+export const TAGS = "tags";
+export const USERS = "users";
+export const CLIENT_REVIEWS = "testimonials";
 
 export const NAVBAR_LINKS = [
   {
-    href: '/',
-    name: 'Home',
+    href: "/",
+    name: "Home",
     match: false,
   },
   // {
@@ -27,85 +27,85 @@ export const NAVBAR_LINKS = [
   //   name: 'Services',
   //   match: true,
   // },
-  // {
-  //   href: '/blog',
-  //   name: 'Blog',
-  //   match: true,
-  // },
+  {
+    href: "/blog",
+    name: "Blog",
+    match: true,
+  },
   // {
   //   href: '/client-reviews',
   //   name: 'Book a Free Consultation',
   //   match: false,
   // },
-  // {
-  //   href: '/contact-us',
-  //   name: 'Contact Us',
-  //   match: false,
-  // },
-]
+  {
+    href: "/contact-us",
+    name: "Contact Us",
+    match: false,
+  },
+];
 
 export const SIDEBAR_LINKS = [
   {
     id: 1,
-    href: '/dashboard/posts/new',
-    name: 'New Post',
-    icon: 'fa fa-plus',
+    href: "/dashboard/posts/new",
+    name: "New Post",
+    icon: "fa fa-plus",
     nested: true,
     disabled: false,
   },
   {
     id: 2,
-    href: '/dashboard/posts',
-    name: 'Posts',
-    icon: 'fa fa-newspaper',
+    href: "/dashboard/posts",
+    name: "Posts",
+    icon: "fa fa-newspaper",
     nested: true,
     disabled: false,
   },
   {
     id: 3,
-    href: '/dashboard/services',
-    name: 'Services',
-    icon: 'fa fa-list-check',
+    href: "/dashboard/services",
+    name: "Services",
+    icon: "fa fa-list-check",
     nested: true,
     disabled: false,
   },
   {
     id: 4,
-    href: '/dashboard/client-reviews',
-    name: 'Client Reviews',
-    icon: 'fa fa-star',
+    href: "/dashboard/client-reviews",
+    name: "Client Reviews",
+    icon: "fa fa-star",
     nested: true,
     disabled: false,
   },
   {
     id: 5,
-    href: '/dashboard/categories',
-    name: 'Categories',
-    icon: 'fa fa-folder-tree',
+    href: "/dashboard/categories",
+    name: "Categories",
+    icon: "fa fa-folder-tree",
     nested: false,
     disabled: false,
   },
   {
     id: 6,
-    href: '/dashboard/tags',
-    name: 'Tags',
-    icon: 'fa fa-hashtag',
+    href: "/dashboard/tags",
+    name: "Tags",
+    icon: "fa fa-hashtag",
     nested: false,
     disabled: false,
   },
   {
     id: 7,
-    href: '/dashboard/users',
-    name: 'Users',
-    icon: 'fa fa-person',
+    href: "/dashboard/users",
+    name: "Users",
+    icon: "fa fa-person",
     nested: false,
     disabled: false,
   },
   {
     id: 8,
-    href: '/dashboard/media',
-    name: 'Media',
-    icon: 'fa fa-image',
+    href: "/dashboard/media",
+    name: "Media",
+    icon: "fa fa-image",
     nested: false,
     disabled: false,
   },
@@ -117,4 +117,4 @@ export const SIDEBAR_LINKS = [
   //   nested: false,
   //   disabled: true,
   // },
-]
+];
