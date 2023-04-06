43a1f34b9ad570a1df3c409c5da290941d55fdb3
aditi <aditi152003@gmail.com>
2023-04-06 20:53:33 +0530

    swipper section title

--- components/SwiperSection.jsx
+++ components/SwiperSection.jsx
@@ -30,7 +30,7 @@ const SwiperSection = () => {
                 <div>Welcome to Azra - Your Personal Dietitian</div>
               </div>
               <div className="lg:text-7xl font-title w-full mb-4 sm:mb-2 lg:mb-4 sm:text-xl text-LG">
-                Easy to Follow <br /> "Customized Diet Plans"
+                Easy to Follow <br /> &quot;Customized Diet Plans&quot;
               </div>
               <div className="hidden sm:block text-xs lg:text-xl text-black mb-4 sm:mb-2 lg:mb-4 sm:text-md  text-[10px]">
                 Weight Loss | Weight Gain | Hypertension | Pregnancy{" "}
